feb37a137c4632c80e5858dca8f766df2e49c906
KE-C02SY0MJGY25 <ke-c02sy0mjgy25@KE-C02SY0MJGY25s-MacBook-Pro.local>
2018-09-25 10:03:25 +0300

    [Chore #160577597] Update to path from url schema

--- authors/urls.py
+++ authors/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-
-    url(r'^api/', include('authors.apps.authentication.urls', namespace='authentication')),
+    path('admin/', admin.site.urls),
+    path('api/', include('authors.apps.authentication.urls', namespace='authentication')),
 ]
 
