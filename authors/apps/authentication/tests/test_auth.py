b38ba59db83b5393377923cb67a2c89c722021e4
Allan Imire <imireallan@gmail.com>
2018-09-25 22:06:51 +0300

    [chore #160577595] testing the auth endpoints

--- authors/apps/authentication/tests/test_auth.py
+++ authors/apps/authentication/tests/test_auth.py
@@ -1,33 +1,40 @@
 from django.test import TestCase
 from rest_framework import status
+from .helpers import register_user, login_user
 
 class ViewsTestCase(TestCase):
     """Tests if a user can be registered suessfully with username, email and password"""
-    def SetUp(self):
-        self.user = {
-            "username": "Yahya Hassan",
-            "email": "hassan@gmail.com",
-            "password": "pass1234"
-         }
-        self.response = self.client.post(
-            'users/', 
-            self.user,
-            format="json"
-         )A
 
     def test_user_can_register(self):
         """"Tests if user can register successfully"""
-        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
-    def test_user_can_login(self):
-        """Test if registered user can logon suessfully"""
-        res = self.client.post(
-            'users/login/',
-            self.user,
-            format="json"
-        )
-        self.asserEqual(self.res.status_code, status.HTTP_200_OK)
-        self.assertIsNotNone(res.data['user'])
-        self.assertIsNotNone(res.data['user']['token'])
+        # registering a new user
+        res = register_user(self)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertIn('User registered successfully', str(res))
+
+    def test_user_cannot_register_twice(self):
+        """Test a user cannot register twice."""
+
+        # registering a new user
+        res = register_user(self)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertIn('User registered successfully', str(res))
+
+        # registering the same user again
+        res = register_user(self)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('User already exists. Please login.', str(res))
+
+    def test_user_cannot_login_before_registering(self):
+        """Test user cannot login before registering"""
+
+        # login a user
+        rv = login_user(self)
+        self.asserEqual(rv.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertIn('User not found. Please register.', str(rv.data))
+
+
+
 
          
