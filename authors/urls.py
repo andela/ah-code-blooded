39fa6ab66d8fef65f3766552e4539d133529621e
“BEV <ke-c02sy0mjgy25@KE-C02SY0MJGY25s-MacBook-Pro.local>
2018-09-25 10:38:16 +0300

    [Chore #160577597] Update auth from urls hema to path

--- authors/urls.py
+++ authors/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authors.apps.authentication.urls', namespace='authentication')),
