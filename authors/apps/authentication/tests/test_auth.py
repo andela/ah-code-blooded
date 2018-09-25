5d2e101e01ed883a6cb6f929bcd8f777c0d36b70
“BEV <ke-c02sy0mjgy25@KE-C02SY0MJGY25s-MacBook-Pro.local>
2018-09-25 16:04:42 +0300

    [Chore #160577595] Tests for Resistration and login

--- authors/apps/authentication/tests/test_auth.py
+++ authors/apps/authentication/tests/test_auth.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from rest_framework import status
+<<<<<<< HEAD
 from .helpers import register_user, login_user
 
 class ViewsTestCase(TestCase):
@@ -36,5 +37,36 @@ class ViewsTestCase(TestCase):
 
 
 
+=======
+
+class ViewsTestCase(TestCase):
+    """Tests if a user can be registered suessfully with username, email and password"""
+    def SetUp(self):
+        self.user = {
+            "username": "Yahya Hassan",
+            "email": "hassan@gmail.com",
+            "password": "pass1234"
+         }
+        self.response = self.client.post(
+            'users/', 
+            self.user,
+            format="json"
+         )A
+
+    def test_user_can_register(self):
+        """"Tests if user can register successfully"""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_can_login(self):
+        """Test if registered user can logon suessfully"""
+        res = self.client.post(
+            'users/login/',
+            self.user,
+            format="json"
+        )
+        self.asserEqual(self.res.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(res.data['user'])
+        self.assertIsNotNone(res.data['user']['token'])
+>>>>>>> [Chore #160577595] Tests for Resistration and login
 
          
