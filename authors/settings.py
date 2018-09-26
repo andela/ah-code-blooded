c0d4a83393b2a472466db6a20ab4a5e50aa368ba
brian okuku <brybzi@gmail.com>
2018-09-26 12:27:45 +0300

    [Chore #160577602]-Configure Authors Haven to use PostgreSQL as DB Engine

--- authors/settings.py
+++ authors/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'HOST': os.getenv('DB_HOST'),
