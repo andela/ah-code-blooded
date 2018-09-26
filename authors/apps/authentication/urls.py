6e25993979d35bfc7661dc553e468c1ec3842e60
Tee Jay <chariss.karunjira@andela.com>
2018-09-26 06:17:53 +0300

    [160749319]Add JWTAuthentication class.

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
-
+app_name = "authentication"
 urlpatterns = [
    path('user/', UserRetrieveUpdateAPIView.as_view()),
    path('users/', RegistrationAPIView.as_view()),
