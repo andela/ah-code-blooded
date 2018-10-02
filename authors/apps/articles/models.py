93d35e767415afb4a78b0e5a97d137d6d50065be
Moses Gitau <gitaumoses4@gmail.com>
2018-10-02 10:56:50 +0300

    [Feature #160577612] Add User model as the author foreign key

--- authors/apps/articles/models.py
+++ authors/apps/articles/models.py
@@ -5,12 +5,15 @@ from authors.apps.core.models import BaseModel
 
 
 class Article(BaseModel):
+    """
+    Model for an article, extends a basemodel since the created and updated times are required
+    """
     slug = models.SlugField(max_length=128, unique=True, db_index=True)
     title = models.CharField(max_length=128)
     description = models.TextField()
     body = models.TextField()
     author = models.ForeignKey(
-        'profiles.Profile',
+        'authentication.User',
         related_name='articles',
         on_delete=models.CASCADE
     )
