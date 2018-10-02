4d3f40ccf4fbda477704b94e66f2e5165149b0a6
Moses Gitau <gitaumoses4@gmail.com>
2018-10-02 10:43:42 +0300

    [Feature #160577612] Add more fields for the articles model

--- authors/apps/articles/models.py
+++ authors/apps/articles/models.py
@@ -6,4 +6,15 @@ from authors.apps.core.models import BaseModel
 
 class Article(BaseModel):
     slug = models.SlugField(max_length=128, unique=True, db_index=True)
-    title = models.CharField()
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    body = models.TextField()
+    author = models.ForeignKey(
+        'profiles.Profile',
+        related_name='articles',
+        on_delete=models.CASCADE
+    )
+    image = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
