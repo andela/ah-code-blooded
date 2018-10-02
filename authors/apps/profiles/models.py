bc80b4e7302964e70704429d4645b955119532b1
chariss <charisschomba@gmail.com>
2018-10-02 10:49:38 +0300

    [Feature #160577611] Setup profile skeleton

--- authors/apps/profiles/models.py
+++ authors/apps/profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from authors.settings import AUTH_USER_MODEL as User
+from authors.settings import AUTH_USER_MODEL
 from cloudinary.models import CloudinaryField
 
 
@@ -10,7 +10,7 @@ class Profile(models.Model):
     This model creates a user profile with bio and
     image field once a user creates an account.
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(default="Update your bio description")
     image = CloudinaryField('image')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -31,7 +31,7 @@ class Profile(models.Model):
         return self.user.username
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     """
     This is triggered when a user is created in order to create their profile.
@@ -40,7 +40,7 @@ def create_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=AUTH_USER_MODEL)
 def save_user_profile(sender, instance, **kwargs):
     """
     This is triggered when a user is saved in order to save their profile.
