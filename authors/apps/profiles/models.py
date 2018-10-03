4cf5fcf775327514505253214354b1beba712568
chariss <charisschomba@gmail.com>
2018-10-03 10:13:51 +0300

    [Feature #160577611] Add profile model

--- authors/apps/profiles/models.py
+++ authors/apps/profiles/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from authors.settings import AUTH_USER_MODEL as User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    This model creater a user profile with bio and
+    image field once a user creates an account.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="Update your bio description")
+    image = CloudinaryField('image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
