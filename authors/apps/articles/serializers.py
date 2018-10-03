af013b067893992a3d61bb3b4e572163da17fe00
Moses Gitau <gitaumoses4@gmail.com>
2018-10-04 00:48:18 +0300

    [Feature #160577612] Refactor serializer for articles to update and render tags and images

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,6 +1,28 @@
 from rest_framework import serializers
 
-from authors.apps.articles.models import Article, Tag
+from authors.apps.articles.models import Article, Tag, ArticleImage
+
+
+class ArticleImageSerializer(serializers.ModelSerializer):
+    """
+    Serialize the Article image
+    """
+
+    class Meta:
+        model = ArticleImage
+        fields = ['image']
+
+
+class ArticleImageField(serializers.RelatedField):
+    def get_queryset(self):
+        return ArticleImage.objects.all()
+
+    def to_internal_value(self, data):
+        image = ArticleImage(image=data)
+        return image
+
+    def to_representation(self, value):
+        return value.image
 
 
 class TagField(serializers.RelatedField):
@@ -15,6 +37,7 @@ class TagField(serializers.RelatedField):
         tag, created = Tag.objects.get_or_create(
             tag=data,
         )
+
         return tag
 
     def to_representation(self, value):
@@ -39,17 +62,30 @@ class ArticleSerializer(serializers.ModelSerializer):
         required=True,
         allow_blank=False
     )
-    image = serializers.CharField(required=False)
+    published = serializers.BooleanField(required=False)
+    images = ArticleImageField(source='articleimage_set', many=True, required=False)
+
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
+    tags = TagField(many=True, required=False)
 
     # tagList = TagField(many=True, required=False)
 
     class Meta:
         model = Article
 
-        fields = ['slug', 'title', 'description', 'body', 'author', 'image',
-                  'created_at', 'updated_at']
+        fields = [
+            'slug',
+            'title',
+            'description',
+            'body',
+            'published',
+            'author',
+            'images',
+            'created_at',
+            'updated_at',
+            'tags'
+        ]
         read_only_fields = ('slug', 'author',)
 
     def create(self, validated_data):
@@ -59,7 +95,20 @@ class ArticleSerializer(serializers.ModelSerializer):
         :param validated_data:
         :return:
         """
+        tags = validated_data.pop('tags', [])
+        images = validated_data.pop('articleimage_set', [])
+
         article = Article.objects.create(**validated_data)
+
+        for tag in tags:
+            article.tags.add(tag)
+
+        for image in images:
+            image.article = article
+            # avoid having two similar images in the same article
+            if not ArticleImage.objects.filter(article__slug=article.slug,
+                                               article__articleimage__image=image):
+                image.save()
 
         return article
 
@@ -70,24 +119,35 @@ class ArticleSerializer(serializers.ModelSerializer):
         :param validated_data:
         :return:
         """
+
+        tags = validated_data.pop('tags', [])
+        images = validated_data.pop('articleimage_set', [])
+
+        instance.tags.clear()
+        for tag in tags:
+            instance.tags.add(tag)
+
+        for image in images:
+            image.article = instance
+            if not ArticleImage.objects.filter(article__slug=instance.slug,
+                                               article__articleimage__image=image):
+                image.save()
+
         for (key, value) in validated_data.items():
             setattr(instance, key, value)
+
+        instance.save()
+
         return instance
 
 
 class TagSerializer(serializers.ModelSerializer):
+    """
+    Validate the tag model
+    """
     tag = serializers.CharField(required=True,
                                 max_length=128)
 
     class Meta:
         model = Tag
-
-    def create(self, validated_data):
-        tag = Tag.objects.create(**validated_data)
-        return tag
-
-    def update(self, instance, validated_data):
-        for (key, value) in validated_data.items():
-            setattr(instance, key, value)
-
-        return instance
+        fields = ['tag']
