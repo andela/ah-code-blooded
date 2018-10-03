d8584cc0b2b8fedc503d2e4b9281c5c665885843
Moses Gitau <gitaumoses4@gmail.com>
2018-10-04 00:45:49 +0300

    [Feature #160577612] Add Tag and ArticleImage in admin site

--- authors/apps/articles/admin.py
+++ authors/apps/articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from authors.apps.articles.models import Article, Tag
+from authors.apps.articles.models import Article, Tag, ArticleImage
 
 admin.site.register(Article)
 admin.site.register(Tag)
+admin.site.register(ArticleImage)
