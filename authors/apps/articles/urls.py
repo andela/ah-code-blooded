bae223fb7ffc62f53489600943ef8643c922c9eb
= <=>
2018-10-04 15:55:54 +0300

    [Feature #160577613] Add ratings url path

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from authors.apps.articles.views import ArticleAPIView
+from authors.apps.articles.views import ArticleAPIView, RatingAPIView
 
 app_name = "articles"
 router = DefaultRouter()
 router.register('articles', ArticleAPIView, base_name="articles")
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('articles/<slug>/rate/', RatingAPIView.as_view()),
 ]
