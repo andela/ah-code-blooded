c6f94fc55867a10e117fc66755a81f4dad13c32b
chariss <charisschomba@gmail.com>
2018-10-04 03:49:14 +0300

    [Feature  #160577611] Update profile urls

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProfileListView
+from .views import ProfileListView, ProfileGetView
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('profiles/', ProfileListView.as_view()),
+    path('profiles/<str:username>/',  ProfileGetView.as_view()),
 ]
