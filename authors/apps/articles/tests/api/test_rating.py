75acfea5b77b52c80cdd3dc969c528ace04586b1
= <=>
2018-10-08 16:01:20 +0300

    [Feature #160577613] Add update rating test

--- authors/apps/articles/tests/api/test_rating.py
+++ authors/apps/articles/tests/api/test_rating.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework.reverse import reverse
 
-from authors.apps.articles.models import Article, ArticleRating
 from authors.apps.articles.tests.api.test_articles import BaseArticlesTestCase
 
 
@@ -15,9 +14,9 @@ class TestArticleRating(BaseArticlesTestCase):
                 "rating": 3
             }
         }
-        self.string_Rating = {
+        self.bad_Rating = {
             "rating": {
-                "rating": "5"
+                "rating": 7
             }
         }
         self.rating_user = {
@@ -28,12 +27,10 @@ class TestArticleRating(BaseArticlesTestCase):
             }
         }
 
-
     def test_user_can_rate_available_article(self):
         """
         User who can rate article has to be authenticated
         """
-        article = self.create_article()['slug']
         slug = self.create_article()['slug']
         rating = self.articleRating
         self.register_and_login(self.rating_user)
@@ -45,7 +42,6 @@ class TestArticleRating(BaseArticlesTestCase):
         """
         If article does not exist, user cannot rate it
         """
-        article = None
         slug = None
         rating = self.articleRating
         self.register_and_login(self.rating_user)
@@ -58,7 +54,6 @@ class TestArticleRating(BaseArticlesTestCase):
         Writers of articles cannot rate their own articles
         Their articles are rated by other users instead
         """
-        article = self.create_article()['slug']
         slug = self.create_article()['slug']
         rating = self.articleRating
         response = self.client.post(reverse("articles:rate-article", kwargs={'slug': slug}), data=rating, format="json")
@@ -70,7 +65,6 @@ class TestArticleRating(BaseArticlesTestCase):
         Users should not rate articles more than once
         Instead they can edit ratings
         """
-        article = self.create_article()['slug']
         slug = self.create_article()['slug']
         rating = self.articleRating
         self.register_and_login(self.rating_user)
@@ -83,11 +77,19 @@ class TestArticleRating(BaseArticlesTestCase):
         """
         For a user to rate an article, they have to be authenticated
         """
-        article = self.create_article()['slug']
         slug = self.create_article()['slug']
         rating = self.articleRating
         self.logout()
         response = self.client.post(reverse("articles:rate-article", kwargs={'slug': slug}), data=rating, format="json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        
 
+    def test_rating_should_be_between_one_and_five(self):
+        """
+        Rating should be in a range between one and five
+        """
+        slug = self.create_article()['slug']
+        rating = self.bad_Rating
+        self.register_and_login(self.rating_user)
+        response = self.client.post(reverse("articles:rate-article", kwargs={'slug': slug}), data=rating, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn(b"Rating should be a number between 1 and 5!", response.content)
