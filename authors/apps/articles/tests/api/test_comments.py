96169c634a50c81a29089312764fed14349c2f67
Yahya Hussein <husain.host@gmail.com>
2018-10-09 21:19:33 +0300

    [feature-#160577614]Add test user can create comment

--- authors/apps/articles/tests/api/test_comments.py
+++ authors/apps/articles/tests/api/test_comments.py
@@ -1,9 +1,30 @@
-import json
-import random
-import string
-
-from django.utils.text import slugify
 from rest_framework import status
 from rest_framework.reverse import reverse
+from authors.apps.articles.tests.api.test_articles import BaseArticlesTestCase
 
-from authors.apps.authentication.tests.api.test_auth import AuthenticatedTestCase
+
+class TestArticleComment(BaseArticlesTestCase):
+    """This class tests for comment article"""
+
+    def setUp(self):
+        super().setUp()
+        self.register()
+        self.comment = {"comment": {"body": "comment on this "}}
+        self.user = {
+            "user": {
+                "username": "tester001",
+                "email": "test@example.com",
+                "password": "@secret123"
+            }
+        }
+
+    def test_create_comment(self):
+        """Authenticated user can add comment"""
+        self.register_and_login(self.user)
+        slug = self.create_article()['slug']
+        comment = self.comment
+        response = self.client.post(
+            reverse("articles:comments", kwargs={'slug': slug}),
+            data=comment,
+            format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
