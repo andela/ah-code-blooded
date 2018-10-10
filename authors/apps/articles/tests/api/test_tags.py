3c7b98697247d77a144defad1bdb15661c91f02b
Moses Gitau <gitaumoses4@gmail.com>
2018-10-11 00:08:02 +0300

    [Feature #160577620] Add tests for tag removal

--- authors/apps/articles/tests/api/test_tags.py
+++ authors/apps/articles/tests/api/test_tags.py
@@ -6,7 +6,7 @@ from rest_framework.reverse import reverse
 from authors.apps.articles.tests.api.test_articles import BaseArticlesTestCase
 
 
-class TagCreationTestCase(BaseArticlesTestCase):
+class BaseTagsTestCase(BaseArticlesTestCase):
 
     def setUp(self):
         super().setUp()
@@ -54,6 +54,9 @@ class TagCreationTestCase(BaseArticlesTestCase):
         """
         return self.client.get(self.generate_url(slug or self.article['slug']))
 
+
+class TagCreationTestCase(BaseTagsTestCase):
+
     def test_user_can_tag_article(self):
         """
         Make sure a user can add tags to their own article
@@ -99,3 +102,52 @@ class TagCreationTestCase(BaseArticlesTestCase):
         for tag in self.tags['tags']:
             # ensure each tag appears only once
             self.assertEqual(tags.count(tag), 1)
+
+
+class TagRemovalTestCase(BaseTagsTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.article_tags = self.tag_article()
+
+    def tag_article(self, slug=None, tags=None):
+        """
+        Overridden method in order to return the tags of an article after tagging
+        :param slug:
+        :param tags:
+        :return:
+        """
+        response = super().tag_article(slug, tags)
+        return json.loads(response.content)['data']['tags']
+
+    def test_user_can_remove_tags_from_an_article(self):
+        """
+        Ensure a user can remove tags from an article
+        :return:
+        """
+        response = self.un_tag_article()
+        tags = json.loads(response.content)['data']['tags']
+
+        for tag in self.tags:
+            # check that the tags do not exist after being removed
+            self.assertNotIn(tag, tags)
+
+    def test_unauthenticated_user_cannot_remove_tags(self):
+        """
+        Ensure a user that is not authenticated cannot remove tags from an article
+        :return:
+        """
+        self.logout()
+        response = self.un_tag_article()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_only_owner_can_remove_tags_from_an_article(self):
+        """
+        Ensure other users cannot un_tag an article
+        :return:
+        """
+        self.register_and_login(self.user2)
+        response = self.un_tag_article()
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
