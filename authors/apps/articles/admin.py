e41d2bc06f5aefb84c1406e34c3963125081ca97
chariss <charisschomba@gmail.com>
2018-10-12 20:46:43 +0300

    [Feature #160577626] Register model and addedreport view url

--- authors/apps/articles/admin.py
+++ authors/apps/articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from authors.apps.articles.models import Article, Tag
+from authors.apps.articles.models import Article, Tag, Violation
 
 admin.site.register(Article)
 admin.site.register(Tag)
+admin.site.register(Violation)
--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -5,6 +5,8 @@ from authors.apps.articles.views import (
     ArticleAPIView, ArticleTagsAPIView, LikeAPIView, DislikeAPIView, ReactionsAPIView
 )
 from authors.apps.articles.views import ArticleAPIView, ArticleTagsAPIView, RatingAPIView
+from authors.apps.articles.views import ReportApiView
+from authors.apps.articles.views import ArticleAPIView
 
 app_name = "articles"
 router = DefaultRouter()
@@ -13,9 +15,5 @@ router.register('articles', ArticleAPIView, base_name="articles")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('articles/<slug>/tags/', ArticleTagsAPIView.as_view(), name="article-tags"),
-    path('articles/<str:slug>/like/', LikeAPIView.as_view(), name='like'),
-    path('articles/<str:slug>/unlike/', DislikeAPIView.as_view(), name='dislike'),
-    path('articles/<str:slug>/reactions/', ReactionsAPIView.as_view(), name='reactions'),
-    path('articles/<slug>/rate/', RatingAPIView.as_view(), name='rate-article'),
+    path('report/', ReportApiView.as_view())
 ]
