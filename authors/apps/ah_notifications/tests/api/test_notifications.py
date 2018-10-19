93cf62b8e88f092d0aa466a520f01d8e0214648a
= <=>
2018-10-19 07:47:38 +0300

    [Feature #160577618] Add test favorite notification

--- authors/apps/ah_notifications/tests/api/test_notifications.py
+++ authors/apps/ah_notifications/tests/api/test_notifications.py
@@ -266,3 +266,49 @@ class ArticleNotificationTestCase(BaseArticlesTestCase, BaseNotificationsTestCas
         self.login(self.user2)
         status_code, data = self.get(notification_type='unsent')
         self.assertEqual(data['data']['count'], 1)
+
+    def test_author_gets_notification_upon_article_favoriting(self):
+            """
+            Ensure a user gets a notification in their unread box
+            :return:
+            """
+            # login another user to favorite
+            self.register_and_login(self.user2)
+
+            # login as first user and create an article
+            self.login(self.user)
+            self.create_article(published=True)
+            slug = self.create_article()['slug']
+
+            # login as second user, favorite
+            self.login(self.user2)
+            self.client.post(reverse(
+                'articles:favourite_article', kwargs={'slug': slug}), format='json')
+            #login as first user and check notification
+            self.login(self.user)
+            status_code, data = self.get(notification_type='unsent')
+            self.assertEqual(data['data']['count'], 1)
+
+    # def test_author_gets_notification_upon_article_commenting(self):
+    #         """
+    #         Ensure a user gets a notification in their unread box
+    #         :return:
+    #         """
+    #         # login another user to favorite
+    #         self.register_and_login(self.user2)
+
+    #         # login as first user and create an article
+    #         self.login(self.user)
+    #         self.create_article(published=True)
+    #         slug = self.create_article()['slug']
+
+    #         # login as second user, comment on article
+    #         self.login(self.user2)
+    #         comment = {"comment": {"body": "comment on this "}}
+    #         response = self.client.get(reverse(
+    #             "articles:comments", kwargs={'slug': slug}), data=comment, format="json")
+    #         #login as first user and check notification
+    #         self.login(self.user)
+    #         status_code, data = self.get(notification_type='unsent')
+    #         self.assertEqual(data['data']['count'], 1)
+
